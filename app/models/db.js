8beb8fe2d39b495b71f2a74badfedac6428c79d7
Shubham Lal <shubhamlal@nimapinfotech.com>
2020-07-18 13:04:44 +0530

    Added Mysqlconnection pool to avoid concorrent connection error

--- app/models/db.js
+++ app/models/db.js
@@ -2,17 +2,26 @@ const mysql = require("mysql");
 const dbConfig = require("../config/db.config");
 
 // Create a connection to the database
-const connection = mysql.createConnection({
-  host: dbConfig.host,
-  user: dbConfig.username,
-  password: dbConfig.password,
-  database: dbConfig.database
+// const connection = mysql.createConnection({
+//     host: dbConfig.host,
+//     user: dbConfig.username,
+//     password: dbConfig.password,
+//     database: dbConfig.database
+// });
+
+const connection = mysql.createPool({
+    connectionLimit: 100, //important
+    host: dbConfig.host,
+    user: dbConfig.username,
+    password: dbConfig.password,
+    database: dbConfig.database,
+    debug: false
 });
 
 // open the MySQL connection
-connection.connect(error => {
-  if (error) throw error;
-  console.log("Successfully connected to the database.");
-});
+// connection.connect(error => {
+//     if (error) throw error;
+//     console.log("Successfully connected to the database.");
+// });
 
 module.exports = connection;
